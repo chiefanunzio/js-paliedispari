ef312b5979b9a93b18ec1a0f50e5c9f0c7973ebb
chiefanunzio <chiefa.nunzio@gmail.com>
2021-03-12 15:21:22 +0100

    ex 1 finito

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-
+// ex 1
 function parolaPalind(val){
 
   return val.split("").reverse().join("");
-
+  // divido i caratteri, li inverto e li riunisco
 }
 var parola = prompt('dimmi una parola');
 var res = parolaPalind(parola);
-console.log(res);
+// richiamo la funzione
+if (parola == res){
+  console.log('la parola é palindroma');
+}else{
+  console.log('ritenta');
+}
+// valuto se sono uguali
