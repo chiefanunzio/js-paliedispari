38922a6a85e3d6b499a7d2b7345eb67bbce07699
chiefanunzio <chiefa.nunzio@gmail.com>
2021-03-12 16:09:31 +0100

    ex 2 completo!

--- js/script.js
+++ js/script.js
@@ -1,15 +1,44 @@
-// ex 1
-function parolaPalind(val){
+// // ex 1
+// function parolaPalind(val){
+//
+//   return val.split("").reverse().join("");
+//   // divido i caratteri, li inverto e li riunisco
+// }
+// var parola = prompt('dimmi una parola');
+// var res = parolaPalind(parola);
+// // richiamo la funzione
+// if (parola == res){
+//   console.log('la parola é palindroma');
+// }else{
+//   console.log('ritenta');
+// }
+// // valuto se sono uguali
 
-  return val.split("").reverse().join("");
-  // divido i caratteri, li inverto e li riunisco
+
+function rndNumber(min, max) {
+  var minRnd = min;
+  var maxRnd = max - minRnd + 1;
+  var rnd = Math.floor(Math.random() * 5) + 1;
+
+  return rnd;
 }
-var parola = prompt('dimmi una parola');
-var res = parolaPalind(parola);
-// richiamo la funzione
-if (parola == res){
-  console.log('la parola é palindroma');
+
+var req = prompt('pari o dispari ?');//requisiti per la vittoria
+var numUtente = parseInt(prompt('dimmi un numero'));//numero del player
+var numIa = rndNumber();//numero generato dal pc
+
+var res = numUtente + numIa;
+if(res % 2 == 0){
+  if(req == 'pari'){
+    console.log('hai vinto con il pari');
+  }else{
+    console.log('ha vinto il pc con il pari');
+  }
 }else{
-  console.log('ritenta');
+  if(req == 'dispari'){
+    console.log('hai vinto con il dispari');
+  }else{
+    console.log('ha vinto il pc con il dispari');
+  }
+
 }
-// valuto se sono uguali
